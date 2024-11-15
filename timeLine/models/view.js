5731992dc03d6952a1209c735131e484ad7560a5
Rerurate514 <rerurateyuto@gmail.com>
2024-11-15 21:32:25 +0900

    vireクラスの作成

--- timeLine/models/view.js
+++ timeLine/models/view.js
@@ -1,3 +1,27 @@
+/**
+ * Viewクラス - abstruct ---
+ * 必ずwrapViewをオーバーライドしてViewをカスタマイズしてください。
+ */
 class View {
-    
+    constructor(child) {
+        if (this.constructor === View) {
+            throw new TypeError('このクラスをインスタンス化しないでください。これは抽象クラスです。');
+        }
+
+        this.view = this.wrapView(child)
+    }
+
+    checkViewComponents(child){
+        if(typeof child !== View){
+            throw new Error("childプロパティには必ずViewオブジェクトを格納してください。")
+        }
+    }
+
+    /**
+     * 必ずこのwrapViewをオーバーライドしてViewをカスタマイズしてください。
+     * 
+     * @param {View} child
+     * @returns {View}  
+     */
+    wrapView(child){ }
 }
