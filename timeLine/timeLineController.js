eaf9f66829a77a225e50550bca62e8f155046ddd
Rerurate514 <rerurateyuto@gmail.com>
2024-11-16 16:57:42 +0900

    コントローラの作成

--- timeLine/timeLineController.js
+++ timeLine/timeLineController.js
@@ -1,3 +1,9 @@
 class TimeLineController{
-    
+    show(){
+        let assembler = new ViewAssembler();
+        let timeline = assembler.assembleView();
+
+        let container = document.getElementById("timeline-Container");
+        container.appendChild(timeline);
+    }
 }
