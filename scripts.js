73164c1782120e552d8c556a026d9e2a254c5368
Rerurate514 <rerurateyuto@gmail.com>
2024-11-19 02:39:45 +0900

    コードを動作させるように実装

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-const tableButton = document.querySelector(".show-bus-table");
+(function() {
+    const tableButton = document.getElementByClassName(".show-bus-table");
 
-tableButton.addEventListener("click", function () {
-    //ここがエントリポイントとして作成    
+    tableButton.addEventListener("click", function () {
+        //ここがエントリポイントとして作成    
+
+        let controller = new TimeLineController();
+        controller.show();
+
+        console.log("sas")
+    })
 })
